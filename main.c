96e7763739fcf4b52998a4beaa6d0f64acc4e6d3
jakubforman <me@jakubforman.eu>
2024-03-25 16:08:23 +0100

    Enhance game with battle, shop, and statistics functionalities
    The game has been improved with the addition of the statistics, battle, and shop functionalities. Users can now view their stats such as health, energy, and xp. The battle function has been updated to randomly decide win or lose and adjust player's stats accordingly. The shop function enables players to purchase items like health and energy.

--- main.c
+++ main.c
@@ -6,40 +6,84 @@ void game_init();
 
 double health = 10.0;
 
+int energy = 100;
+
+int xp = 0;
+
+/**
+ * Výpis základních statistik
+ */
+void stats() {
+    // výpis statistik
+    printf("-----------------------------------------------\n");
+    // výpis statistik na řádek
+    printf("HP: %lf  ENERGY: %d  XP: %d\n", health, energy, xp);
+    printf("-----------------------------------------------\n\n");
+}
+
+/**
+ * Výpis hlavního menu
+ */
 void main_menu() {
+    // výpis menu
     printf("------------------------------------------\n");
-    printf("Vitej ve hre ORG GAME\n\n");
+    printf("Vitej ve hre ORBIS GAME\n\n");
     printf("MENU:\n");
     printf("1. Nova hra\n");
     printf("2. Ukoncit\n");
     printf("------------------------------------------\n");
 }
 
+/**
+ * Soubojový systém s hláškai
+ * @return
+ */
 int battle() {
+    // výpis statistik uživatele
+    stats();
+    printf("\n");
+
+    // menu soubojového systému
     printf("-----------------\n");
     printf("Vyber akci:\n");
     printf("1 - utok\n");
     printf("2 - obrana\n");
     printf("-----------------\n");
 
+    // definice proměnné a získání dat od uživatele z klávesnice
     int choice;
     scanf("%d", &choice);
 
+    // definice náhodné 1 nebo 0 při souboji
     srand(time(0));
     int random = rand() % 2; // 0, 1
 
+    // reagování na výhru nebo prohru
     if (random == 1) {
         printf("Souboj si vyhral :) !\n");
+        xp += 30; // TODO: random 10 - 50
     } else {
         printf("Souboj si prohrál :( !\n");
+        health--; // TODO: přepsat na random - 1 - 2 životy v des. čísle
     }
 
+    energy -= 25; // TODO: random 5 - 30
+
+    // vrácení dat o výhře nebo prohře
     return random;
 }
 
+/**
+ * Otevření menu obchodu s akcemi
+ */
 void shop_init() {
     printf("Vitejte v obchode\n\n");
 
+    // výpis tabulky statistik
+    stats();
+    printf("\n");
+
+    // výpis menu obchodu
     printf("Vyber si danou akci.\n");
     printf("-------------------------------------\n");
     printf("1 - koupit zivot\n");
@@ -47,21 +91,27 @@ void shop_init() {
     printf("3 - zpet\n");
     printf("-------------------------------------\n");
 
+    // definice proměnné a získání dat od uživatele z klávesnice
     int choice;
     scanf("%d", &choice);
 
     switch (choice) {
+        // uživatel zvolil koupění živata
         case 1:
-            if (health >= 10) {
+            // pokud je život větší než 9, nastav na 10, jinak přičti +1 k životu
+            if (health >= 9) {
                 printf("Mas plny pocet zivotu\n\n");
+                health = 10;
             } else {
                 printf("Koupil jsi život\n\n");
                 health++;
             }
-
+            // opětovné otevření shopu
+            shop_init();
             break;
         case 2:
             printf("Koupil jsi energii\n\n");
+            shop_init();
             break;
         case 3:
             game_init();
@@ -71,9 +121,17 @@ void shop_init() {
     }
 }
 
+/**
+ * Inicializace hry
+ */
 void game_init() {
     printf("Vitejte ve hre\n\n");
 
+    // výpis statistik uživatele
+    stats();
+    printf("\n");
+
+    // výpis menu hry
     printf("Jsi na krizovatce, rozhodni se, kam pujdes.\n");
     printf("-------------------------------------\n");
     printf("1 - cesta do lesa\n");
@@ -81,19 +139,21 @@ void game_init() {
     printf("3 - cesta do obchodu\n");
     printf("-------------------------------------\n");
 
+    // definice proměnné a získání dat od uživatele z klávesnice
     int choice;
     scanf("%d", &choice);
 
+    // reakce na danou akci uživatele
     switch (choice) {
         case 1:
             printf("Potkal si pavouka, budes bojovat\n\n");
-            int result = battle(); // 1 = vyhra, 0 = prohra
-            // TODO: pokud vyhraje přidat XP, pokud prohraje, ubrat životy
+            battle(); // 1 = vyhra, 0 = prohra
+            game_init();
             break;
         case 2:
             printf("Potkal jsi kostlivce, budeš bojovat\n\n");
-            int result2 = battle(); // 1 = vyhra, 0 = prohra
-            // TODO: pokud vyhraje přidat XP, pokud prohraje, ubrat životy
+            battle(); // 1 = vyhra, 0 = prohra
+            game_init();
             break;
         case 3:
             shop_init();
@@ -105,11 +165,14 @@ void game_init() {
 }
 
 int main() {
+    // zapnutí menu
     main_menu();
 
+    // definice proměnné a získání dat od uživatele z klávesnice
     int choice;
     scanf("%d", &choice);
 
+    // reagování na uživatelovu volbu
     switch (choice) {
         case 1:
             game_init();
